529d34d6addf4404ffe4395ec292b4971271429c
afmagee42 <rzg0@cdc.gov>
2025-09-09 12:23:57 -0700

    typo

--- src/cfa_subgroup_imputer/imputer.py
+++ src/cfa_subgroup_imputer/imputer.py
@@ -99,7 +99,7 @@ class Disaggregator:
 
     def __call__(self, map: GroupMap) -> GroupMap:
         """
-        Impute and disaggregate the given group map.
+        Impute and aggregate the given group map.
         """
 
         sub_to_super = map.sub_to_super
